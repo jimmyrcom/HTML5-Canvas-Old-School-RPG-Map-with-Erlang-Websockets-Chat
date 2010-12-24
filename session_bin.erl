1c68c63e51eea76d475ce39baf6da5c807d6dbe5
Jimmy Ruska <jimmyruska@gmail.com>
2010-12-23 23:39:32 -0600

    php session parser

--- session_bin.erl
+++ session_bin.erl
@@ -12,47 +12,50 @@
 
 %% You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-
+%binary input
 session(Session1) ->
-    Session = re:replace(Session1,"[^a-z0-9]+","",[global,{return,list}]),
+    Session = re:replace(Session1,<<"[^a-z0-9]+">>,<<"">>,[global,{return,binary}]),
 
     case byte_size(Session) of
         26 -> void;
         _ -> throw("invalid session"),u:trace("Invalid Session")
     end,
 
-    case file:read_file(["/var/lib/php5/sess_",Session]) of
+    case file:read_file(["/var/lib/php5/sess_",binary_to_list(Session1)]) of
         {ok,Bin} -> parse(Bin);
-        {error,_} -> fail
+        {error,_} -> throw("Could Not Load File")
     end.
-
 parse(<<>>) -> fail;
-parse(S) -> parseKey(S,[],[]).
+parse(S) -> parseKey(S,<<>>,[]).
 
 parseKey(<<>>,_,List) -> lists:reverse(List);
 parseKey(<<$\|,S/binary>>,Key,List) -> 
-    parseType(S,lists:reverse(Key),List);
+    parseType(S,Key,List);
 parseKey(<<C,S/binary>>,Key,List) ->
-    parseKey(S,[C|Key],List).
+    parseKey(S,<<Key/binary,C>>,List).
 
 parseType(<<>>,_,_) -> fail;
 parseType(<<C,S/binary>>,Key,List) ->
     case C of
         $i ->
             <<_,S1/binary>> = S,
-            parseInt(S1,Key,[],List);
+            parseInt(S1,Key,<<>>,List);
         $s ->
             <<$:,S1/binary>> = S,
-            {Amount,S2}=parseStrLen(S1,[]),
-            parseString(Amount,S2,Key,[],List)
+            parseStrLen(S1,<<>>,Key,List)
     end.
 
 %parseInt([],Key,Value,List) -> [{Key,list_to_integer(Value)}|List];
-parseInt(<<$;,S/binary>>,Key,Value,List) -> parseKey(S,[],[{Key,list_to_integer(Value)}|List]);
-parseInt(<<C,S/binary>>,Key,Value,List) -> parseInt(S,Key,[C|Value],List).
+parseInt(<<$;,S/binary>>,Key,Value,List) -> parseKey(S,<<>>,[{Key,binary_to_integer(Value,0)}|List]);
+parseInt(<<C,S/binary>>,Key,Value,List) -> parseInt(S,Key,<<Value/binary,C>>,List).
 
-parseStrLen(<<$:,$",T/binary>>,List) -> {list_to_integer(lists:reverse(List)),T};
-parseStrLen(<<C,T/binary>>,List) -> parseStrLen(T,[C|List]).
+parseStrLen(<<$:,$",T/binary>>,Len,Key,List) -> parseString(binary_to_integer(Len,0),T,Key,<<>>,List);
+parseStrLen(<<C,T/binary>>,Len,Key,List) -> parseStrLen(T,<<Len/binary,C>>,Key,List).
 
-parseString(0,<<$",$;,S/binary>>,Key,Value,List) -> parseKey(S,[],[{Key,lists:reverse(Value)}|List]);
-parseString(Amount,<<C,S/binary>>,Key,Value,List) -> parseString(Amount-1,S,Key,[C|Value],List).
+parseString(0,<<$",$;,S/binary>>,Key,Value,List) -> parseKey(S,<<>>,[{Key,Value}|List]);
+parseString(Amount,<<C,S/binary>>,Key,Value,List) -> parseString(Amount-1,S,Key,<<Value/binary,C>>,List).
+
+binary_to_integer(<<>>,Acc) -> Acc;
+binary_to_integer(<<Num:8,Rest/binary>>,Acc) when Num >= 48 andalso Num < 58 ->
+   binary_to_integer(Rest, Acc*10 + (Num-48));
+binary_to_integer(_,Acc) -> exit({badarg,Acc}).
